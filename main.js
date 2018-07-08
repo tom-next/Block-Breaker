fb3f0bf366811d35e6b91bfe0e4859725a981d91
tom <526019128@qq.com>
2018-07-08 11:43:02 +0800

    抽象场景文件

--- main.js
+++ main.js
@@ -17,7 +17,6 @@ var enableDebugMode = function(game, enable) {
     document.querySelector("#id-input-speed").addEventListener("input", function(e) {
         window.fps = Number(e.target.value)
     })
-
 }
 
 var loadLeves = function(game, n) {
@@ -43,81 +42,14 @@ var __main = function() {
     }
     // 异步记载
     var game = Game(30, images, function() {
-        var paddle = Paddle(game)
-        var ball = Ball(game)
-        blocks = loadLeves(game, 1)
-        var score = 100
-        game.registerAction("a", function() {
-            paddle.moveLeft()
-        })
 
-        game.registerAction("d", function() {
-            paddle.moveRight()
-        })
-
-        game.registerAction("f", function() {
-            ball.fire()
-        })
-
+        var s = Scene(game)
         game.update = function() {
-            if(paused) {
-                return;
-            }
-            ball.move()
-            if(paddle.collide(ball)) {
-                ball.rebound()
-            }
-            for (var i = 0; i < blocks.length; i++) {
-                var b = blocks[i]
-                if(b.collide(ball)) {
-                    b.kill()
-                    ball.rebound()
-                }
-            }
+            s.update()
         }
-
         game.draw = function() {
-            // draw backImg
-            game.context.fillStyle = "green";
-            game.context.fillRect(0, 0, game.canvas.width, game.canvas.height);
-
-            game.drawImage(paddle)
-            game.drawImage(ball)
-            for (var i = 0; i < blocks.length; i++) {
-                var b = blocks[i]
-                // draw
-                if(b.alive) {
-                    game.drawImage(b)
-                }
-            }
-            // draw text
-            game.context.fillText("分数: "+score, 10, 290);
+            s.draw()
         }
-
-        var enableDrag = false
-        game.canvas.addEventListener("mousedown", function(event) {
-            var x = event.offsetX
-            var y = event.offsetY
-            // 设置 可以拖动
-            if(ball.hasPoint(x, y)) {
-                enableDrag = true
-            }
-        })
-
-        game.canvas.addEventListener("mousemove", function(event) {
-            var x = event.offsetX
-            var y = event.offsetY
-            if(enableDrag) {
-                ball.x = x
-                ball.y = y
-            }
-        })
-
-        game.canvas.addEventListener("mouseup", function(event) {
-            var x = event.offsetX
-            var y = event.offsetY
-            enableDrag = false
-        })
         enableDebugMode(game, true)
 
     })
---
+++ scene.js
@@ -0,0 +1,80 @@
+var Scene = function(game) {
+    var s = {
+
+    }
+    var paddle = Paddle(game)
+    var ball = Ball(game)
+    blocks = loadLeves(game, 1)
+    var score = 100
+    game.registerAction("a", function() {
+        paddle.moveLeft()
+    })
+
+    game.registerAction("d", function() {
+        paddle.moveRight()
+    })
+
+    game.registerAction("f", function() {
+        ball.fire()
+    })
+    var enableDrag = false
+    game.canvas.addEventListener("mousedown", function(event) {
+        var x = event.offsetX
+        var y = event.offsetY
+        // 设置 可以拖动
+        if(ball.hasPoint(x, y)) {
+            enableDrag = true
+        }
+    })
+
+    game.canvas.addEventListener("mousemove", function(event) {
+        var x = event.offsetX
+        var y = event.offsetY
+        if(enableDrag) {
+            ball.x = x
+            ball.y = y
+        }
+    })
+
+    game.canvas.addEventListener("mouseup", function(event) {
+        var x = event.offsetX
+        var y = event.offsetY
+        enableDrag = false
+    })
+
+    s.update = function() {
+        if(paused) {
+            return;
+        }
+        ball.move()
+        if(paddle.collide(ball)) {
+            ball.rebound()
+        }
+        for (var i = 0; i < blocks.length; i++) {
+            var b = blocks[i]
+            if(b.collide(ball)) {
+                b.kill()
+                ball.rebound()
+            }
+        }
+    }
+
+    s.draw = function() {
+        // draw backImg
+        game.context.fillStyle = "green";
+        game.context.fillRect(0, 0, game.canvas.width, game.canvas.height);
+
+        game.drawImage(paddle)
+        game.drawImage(ball)
+        for (var i = 0; i < blocks.length; i++) {
+            var b = blocks[i]
+            // draw
+            if(b.alive) {
+                game.drawImage(b)
+            }
+        }
+        // draw text
+        game.context.fillText("分数: "+score, 10, 290);
+    }
+    return s
+}
